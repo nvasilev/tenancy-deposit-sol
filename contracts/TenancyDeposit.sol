fd545136958ae6c1add5a9c93ef5c8e7c2f959f2
Nikolay Vasilev <n.v.vasilev@gmail.com>
2018-03-08 19:45:15 +0000

    Attempted simplifying the contract

---
+++ contracts/TenancyDeposit.sol
@@ -0,0 +1,174 @@
+pragma solidity ^0.4.18;
+
+contract TenancyDeposit {
+
+    enum ContractStatus {UNSIGNED, DEPOSIT_REQUIRED, ACTIVE, COMPLETE, DEDUCTION_CLAIM, DEDUCTION_CLAIMED, DISPUTE, DISPUTE_RESOLVED, DONE}
+
+    ContractStatus status = ContractStatus.UNSIGNED;
+
+    uint constant MAX_VALUE = ~uint256(0);
+
+    modifier landlordOnly() {
+        require(landlord == msg.sender);
+        _;
+    }
+
+    modifier tenantOnly() {
+        require(tenant == msg.sender);
+        _;
+    }
+
+    modifier arbiterOnly() {
+        require(arbiter == msg.sender);
+        _;
+    }
+
+    modifier withContractStatus(ContractStatus _status) {
+        require(status == _status);
+        _;
+    }
+
+    address landlord;
+    bool landlordDeductionClaimed;
+    bool landlordDeductionPaid;
+    uint landlordDeductionClaim;
+    address tenant;
+    bool tenantDeductionClaimed;
+    bool tenantDepositReturned;
+    uint tenantDeductionClaim;
+    address arbiter;
+    uint arbiterDeductionClaim;
+    uint expectedDeposit;
+    uint paidDeposit;
+    uint creationDate;
+
+    function TenancyDeposit(address _tenant, address _arbiter, uint _expectedDeposit) public payable
+    {
+        require(_tenant != msg.sender);
+        require(_arbiter != msg.sender);
+        require(_arbiter != _tenant);
+        require(_expectedDeposit > 0 ether);
+        require(status == ContractStatus.UNSIGNED);
+
+        landlord = msg.sender;
+        landlordDeductionClaimed = false;
+        landlordDeductionClaim = 0;
+
+        tenant = _tenant;
+        tenantDeductionClaimed = false;
+        tenantDeductionClaim = 0;
+
+        arbiter = _arbiter;
+        arbiterDeductionClaim = 0;
+
+        expectedDeposit = _expectedDeposit;
+        paidDeposit = 0 ether;
+
+        creationDate = block.timestamp;
+
+        status = ContractStatus.DEPOSIT_REQUIRED;
+    }
+
+    function signContract() public payable
+    tenantOnly
+    withContractStatus(ContractStatus.DEPOSIT_REQUIRED)
+    {
+        require(expectedDeposit <= msg.value);
+        paidDeposit = msg.value;
+        status = ContractStatus.ACTIVE;
+    }
+
+    function expireContract() public payable withContractStatus(ContractStatus.ACTIVE) {
+        require(tenant == msg.sender || landlord == msg.sender);
+        status = ContractStatus.COMPLETE;
+    }
+
+    function tenantClaimDeduction(uint _tenantDeductionClaim) public tenantOnly {
+        require(status == ContractStatus.COMPLETE || status == ContractStatus.DEDUCTION_CLAIM);
+        require(!tenantDeductionClaimed);
+        require(_tenantDeductionClaim <= paidDeposit);
+
+        tenantDeductionClaim = _tenantDeductionClaim;
+        tenantDeductionClaimed = true;
+
+        if (landlordDeductionClaimed) {
+            status = ContractStatus.DEDUCTION_CLAIMED;
+        } else {
+            status = ContractStatus.DEDUCTION_CLAIM;
+        }
+    }
+
+    function landlordClaimDeduction(uint _landlordDeductionClaim) public landlordOnly {
+        require(status == ContractStatus.COMPLETE || status == ContractStatus.DEDUCTION_CLAIM);
+        require(!landlordDeductionClaimed);
+        require(_landlordDeductionClaim <= paidDeposit);
+
+        landlordDeductionClaim = _landlordDeductionClaim;
+        landlordDeductionClaimed = true;
+
+        if (landlordDeductionClaimed) {
+            status = ContractStatus.DEDUCTION_CLAIMED;
+        } else {
+            status = ContractStatus.DEDUCTION_CLAIM;
+        }
+    }
+
+    function withdrawLandlordClaim() public payable landlordOnly {
+        require(status == ContractStatus.DEDUCTION_CLAIMED || status == ContractStatus.DISPUTE_RESOLVED);
+        require(!landlordDeductionPaid);
+
+        if (landlordDeductionClaim != tenantDeductionClaim) {
+            status = ContractStatus.DISPUTE;
+            return;
+        }
+
+        msg.sender.transfer(landlordDeductionClaim);
+        landlordDeductionPaid = true;
+        if (tenantDepositReturned) {
+            status = ContractStatus.COMPLETE;
+        }
+    }
+
+    function withdrawTenantDeposit() public payable tenantOnly {
+        require(status == ContractStatus.DEDUCTION_CLAIMED || status == ContractStatus.DISPUTE_RESOLVED);
+        require(!tenantDepositReturned);
+
+        if (landlordDeductionClaim != tenantDeductionClaim) {
+            status = ContractStatus.DISPUTE;
+            return;
+        }
+
+        uint deduction = landlordDeductionClaim;
+        if (status == ContractStatus.DISPUTE_RESOLVED) {
+            deduction = arbiterDeductionClaim;
+        }
+
+        msg.sender.transfer(paidDeposit - deduction);
+        tenantDepositReturned = true;
+        if (tenantDepositReturned) {
+            status = ContractStatus.COMPLETE;
+        }
+    }
+
+    function resolveDispute(uint claim) public payable arbiterOnly withContractStatus(ContractStatus.DISPUTE) {
+        require(tenantDeductionClaim != landlordDeductionClaim);
+        require(claim <= paidDeposit);
+
+        arbiterDeductionClaim = claim;
+        status = ContractStatus.DISPUTE_RESOLVED;
+        // TODO collect fee
+    }
+
+    function getExpectedDeposit() view public returns (uint) {
+        return expectedDeposit;
+    }
+
+    function getPaidDeposit() view public returns (uint) {
+        return paidDeposit;
+    }
+
+    function getContractStatus() view public returns (ContractStatus) {
+        return status;
+    }
+
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,11 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var TenancyContract = artifacts.require("./TenancyContract.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(TenancyContract,"0xffcf8fdee72ac11b5c542428b35eef5769c409f0",
+                                  "0x22d491bde2303f2f43325b2108d26f1eaba1e32b",
+                                  10,
+                                  20
+      );
 };
